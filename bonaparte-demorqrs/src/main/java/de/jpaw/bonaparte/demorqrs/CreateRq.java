0c4201d70ebf0f36e762ba45017ff3c786cc6e4e
Michael Bischoff <jpaw@online.de>
2012-10-20 07:20:16 +0200

    add missing demorqrs project

---
+++ bonaparte-demorqrs/src/main/java/de/jpaw/bonaparte/demorqrs/CreateRq.java
@@ -0,0 +1,18 @@
+package de.jpaw.bonaparte.demorqrs;
+
+import java.util.UUID;
+import de.jpaw.bonaparte.pojos.rqrs.Request;
+
+public class CreateRq {
+	
+	public static Request createRequest() {
+        UUID myUuid = UUID.randomUUID();
+        Request myRequest = new Request();
+        myRequest.setDuration(0);
+        myRequest.setMessage("Hello, World");
+        myRequest.setSerialNo(0);
+        myRequest.setUniqueId(myUuid);
+		return myRequest;
+	}
+
+}
