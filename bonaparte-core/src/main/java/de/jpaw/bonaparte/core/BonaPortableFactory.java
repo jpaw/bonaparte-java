e64ee73ac1066d88ded2bde3102d24606222578a
Michael Bischoff <jpaw@online.de>
2013-01-24 18:16:29 +0100

    patch from Christian to allow use from JCA connector

--- bonaparte-core/src/main/java/de/jpaw/bonaparte/core/BonaPortableFactory.java
+++ bonaparte-core/src/main/java/de/jpaw/bonaparte/core/BonaPortableFactory.java
@@ -1,23 +1,23 @@
- /*
-  * Copyright 2012 Michael Bischoff
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *   http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+/*
+ * Copyright 2012 Michael Bischoff
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.jpaw.bonaparte.core;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,11 +32,11 @@ public class BonaPortableFactory {
         map.putIfAbsent(name, clatz);
         logger.debug("Factory: registered class {}", name);
     }
-    
+
     // prevent instance creation
     private BonaPortableFactory() {
     }
-    
+
     // generalized factory: create an instance of the requested type. Caches class types.
     // We receive PQCN (partially qualified class name) as parameter.
     // Anything before the last '.' is the Bonaparte package name (which is null is there is no '.').
@@ -49,35 +49,37 @@ public class BonaPortableFactory {
         String FQON = null;
         BonaPortable instance = null;
         int lastDot = name.lastIndexOf('.');
-        if (lastDot == 0 || lastDot >= name.length() - 1)
+        if ((lastDot == 0) || (lastDot >= (name.length() - 1))) {
             throw new MessageParserException(MessageParserException.BAD_OBJECT_NAME,
                     null, -1, name);
+        }
         String myPackage;
         String myClass;
         if (lastDot < 0) {
             myPackage ="";
             myClass = name;
         } else {
-            myPackage = name.substring(0, lastDot); 
+            myPackage = name.substring(0, lastDot);
             myClass = name.substring(lastDot+1);
         }
-        
-        
+
+
         if (packagePrefixMap != null) {
             String mappedPackagePart = packagePrefixMap.get(myPackage);
-            if (mappedPackagePart != null)
+            if (mappedPackagePart != null) {
                 FQON = mappedPackagePart + "." + myClass;
+            }
         }
-        if (FQON == null)
+        if (FQON == null) {
             // prefix by fixed package
             FQON = bonaparteClassDefaultPackagePrefix + "." + name;
-            
+        }
+
         Class<? extends BonaPortable> f = map.get(FQON);
         if (f == null) {
             try {
-                // String classname = "de.adata.server." + name;
                 logger.debug("Factory: loading class {}", FQON);
-                f = Class.forName(FQON).asSubclass(BonaPortable.class);
+                f = Class.forName(FQON, true, Thread.currentThread().getContextClassLoader()).asSubclass(BonaPortable.class);
                 registerClass(FQON, f);
             } catch (ClassNotFoundException e) {
                 logger.error("ClassNotFound exception for {}", FQON);
@@ -95,8 +97,8 @@ public class BonaPortableFactory {
         return instance;
     }
 
-    
-    // auto getters and setters only following 
+
+    // auto getters and setters only following
     public static Map<String, String> getPackagePrefixMap() {
         return packagePrefixMap;
     }
