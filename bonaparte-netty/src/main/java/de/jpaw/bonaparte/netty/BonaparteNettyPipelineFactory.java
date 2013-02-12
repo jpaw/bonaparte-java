676dc8f0df5f7a7f7048d802e3b40558585805c0
Michael Bischoff <jpaw@online.de>
2013-02-12 19:31:57 +0100

    allow error converter

--- bonaparte-netty/src/main/java/de/jpaw/bonaparte/netty/BonaparteNettyPipelineFactory.java
+++ bonaparte-netty/src/main/java/de/jpaw/bonaparte/netty/BonaparteNettyPipelineFactory.java
@@ -1,6 +1,5 @@
 package de.jpaw.bonaparte.netty;
 
-import de.jpaw.bonaparte.core.BonaPortable;
 import io.netty.channel.ChannelInboundMessageHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
@@ -8,15 +7,20 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
 
+import de.jpaw.bonaparte.core.BonaPortable;
+
 public class BonaparteNettyPipelineFactory extends ChannelInitializer<SocketChannel> {
     private final int maximumMessageLength;
     private final ChannelInboundMessageHandlerAdapter<BonaPortable> objectHandler;
-    
-    public BonaparteNettyPipelineFactory(int maximumMessageLength, ChannelInboundMessageHandlerAdapter<BonaPortable> objectHandler) {
+    private final ErrorForwarder errorForwarder;
+
+    public BonaparteNettyPipelineFactory(int maximumMessageLength, ChannelInboundMessageHandlerAdapter<BonaPortable> objectHandler,
+            ErrorForwarder errorForwarder) {
         this.maximumMessageLength = maximumMessageLength;
         this.objectHandler = objectHandler;
+        this.errorForwarder = errorForwarder;
     }
-    
+
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
@@ -24,7 +28,7 @@ public class BonaparteNettyPipelineFactory extends ChannelInitializer<SocketChan
         // Add the text line codec combination first,
         pipeline.addLast("framer", new DelimiterBasedFrameDecoder(maximumMessageLength, false, Delimiters.lineDelimiter()));
         // transmission serialization format
-        pipeline.addLast("decoder", new BonaparteNettyDecoder());
+        pipeline.addLast("decoder", new BonaparteNettyDecoder(errorForwarder));
         pipeline.addLast("encoder", new BonaparteNettyEncoder());
         // and then business logic.
         pipeline.addLast("handler", objectHandler);
