4efb0728282e040d7ece754b7a69464d8629a30c
Michael Bischoff <jpaw@online.de>
2013-02-15 21:21:47 +0100

    update netty release from Alpha to Beta

--- bonaparte-netty/src/main/java/de/jpaw/bonaparte/netty/BonaparteNettyEncoder.java
+++ bonaparte-netty/src/main/java/de/jpaw/bonaparte/netty/BonaparteNettyEncoder.java
@@ -1,29 +1,25 @@
 package de.jpaw.bonaparte.netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.MessageBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.jpaw.bonaparte.core.BonaPortable;
 import de.jpaw.bonaparte.core.ByteArrayComposer;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.MessageBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelHandler.Sharable;
-import io.netty.handler.codec.MessageToMessageEncoder;
 
 @Sharable
-public class BonaparteNettyEncoder extends MessageToMessageEncoder<BonaPortable, ByteBuf> {
+public class BonaparteNettyEncoder extends MessageToMessageEncoder<BonaPortable> {
     private static final Logger logger = LoggerFactory.getLogger(BonaparteNettyEncoder.class);
 
     @Override
     public MessageBuf<BonaPortable> newOutboundBuffer(ChannelHandlerContext ctx) throws Exception {
         return Unpooled.messageBuffer();
-    }
-
-    @Override
-    public boolean isEncodable(Object msg) throws Exception {
-        return msg instanceof BonaPortable;
     }
 
     @Override
