63fc23be494e560aa8287a1bce1235b80a25e7c4
Michael Bischoff <jpaw@online.de>
2013-02-24 19:34:21 +0100

    bugfixes

--- bonaparte-core/src/main/java/de/jpaw/bonaparte/core/StringConverter.java
+++ bonaparte-core/src/main/java/de/jpaw/bonaparte/core/StringConverter.java
@@ -2,6 +2,8 @@ package de.jpaw.bonaparte.core;
 
 import java.util.List;
 
+import de.jpaw.bonaparte.pojos.meta.AlphanumericElementaryDataItem;
+
 /**
  * The StringConverter interface is used to run some preprocessor over objects whih are mapped to type String (Ascii, Unicode, Upper, Lower).
  * 
@@ -11,14 +13,14 @@ import java.util.List;
  */
 public interface StringConverter {
     /** Method invoked for a single String */
-    String convert(String oldValue, int maxLength, boolean isRequired);
+    String convert(String oldValue, final AlphanumericElementaryDataItem meta);
     
     /** Method invoked for a String List. This can be used to alter the list length / add / remove elements after processing.
      * Please note that the converter is only invoked on the list itself, not on the individual elements. */
-    List <String> convertList(List<String> oldList, int maxElementLength, int maxElements, boolean isRequired);
+    List <String> convertList(List<String> oldList, final AlphanumericElementaryDataItem meta);
 
     /** Method invoked for an array of Strings. This can be used to alter the list length / add / remove elements after processing.
      * Please note that the converter is only invoked on the array itself, not on the individual elements. */
-    String [] convertArray(String [] oldArray, int maxElementLength, int maxElements, boolean isRequired);
+    String [] convertArray(String [] oldArray, final AlphanumericElementaryDataItem meta);
 
 }
--- bonaparte-core/src/main/java/de/jpaw/util/StringConverterEmptyToNull.java
+++ bonaparte-core/src/main/java/de/jpaw/util/StringConverterEmptyToNull.java
@@ -4,33 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.jpaw.bonaparte.core.StringConverter;
+import de.jpaw.bonaparte.pojos.meta.AlphanumericElementaryDataItem;
 
 /** Sample implementation of StringConverter interface which replaces empty Strings with nulls */
 
 public class StringConverterEmptyToNull implements StringConverter {
 
     @Override
-    public String convert(String oldValue, int maxLength, boolean isRequired) {
+    public String convert(String oldValue, final AlphanumericElementaryDataItem meta) {
         return (oldValue != null) && oldValue.isEmpty() ? null : oldValue;
     }
 
     @Override
-    public List<String> convertList(List<String> oldList, int maxElementLength, int maxElements, boolean isRequired) {
+    public List<String> convertList(List<String> oldList, final AlphanumericElementaryDataItem meta) {
         if (oldList == null)
             return null;
         List<String> newList = new ArrayList<String>(oldList.size());
         for (String s : oldList) {
-            String newString = convert(s, maxElementLength, isRequired);
+            String newString = convert(s, meta);
             if (newString != null)
                 newList.add(newString);
         }
-        return (newList.isEmpty() && !isRequired) ? null : newList;  // only return null instead of an empty list if the List is required.
+        return (newList.isEmpty() && !meta.getIsRequired()) ? null : newList;  // only return null instead of an empty list if the List is required.
     }
 
     @Override
-    public String[] convertArray(String[] oldArray, int maxElementLength, int maxElements, boolean isRequired) {
+    public String[] convertArray(String[] oldArray, final AlphanumericElementaryDataItem meta) {
         for (int i = 0; i < oldArray.length; ++i)
-            oldArray[i] = convert(oldArray[i], maxElementLength, isRequired);
+            oldArray[i] = convert(oldArray[i], meta);
         return oldArray;  // no conversion of the array itself done here
     }
 
