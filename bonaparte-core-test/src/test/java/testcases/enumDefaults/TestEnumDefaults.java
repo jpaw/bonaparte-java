6d082b5ac739753ee5223d295d46ce6b21370e68
Michael Bischoff <jpaw@online.de>
2014-01-06 22:38:34 +0100

    adding test cases for enum defaults

---
+++ bonaparte-core-test/src/test/java/testcases/enumDefaults/TestEnumDefaults.java
@@ -0,0 +1,27 @@
+package testcases.enumDefaults;
+
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.pojos.enumWithDefaults.ClassWithEnumDefaultsDueToPackageDefaults;
+import de.jpaw.bonaparte.pojos.enumWithDefaults.ClassWithoutDefaults;
+import de.jpaw.bonaparte.pojos.enumWithDefaults.Color;
+import de.jpaw.bonaparte.pojos.enumWithDefaults.Color2;
+
+public class TestEnumDefaults {
+
+    @Test
+    public static void testEnumsWithPackageDefaults() {
+        ClassWithEnumDefaultsDueToPackageDefaults c1 = new ClassWithEnumDefaultsDueToPackageDefaults();
+        assert (c1.getThisShouldBeRed() == Color.RED);
+        assert (c1.getThisShouldAlsoBeRed() == Color2.RED);
+    }
+    
+    @Test
+    public static void testEnumsWithoutDefaults() {
+        ClassWithoutDefaults c1 = new ClassWithoutDefaults();
+        assert (c1.getThisShouldBeRed() == Color.RED);
+        assert (c1.getThisShouldAlsoBeRed() == Color2.RED);
+        assert (c1.getThisShouldBeNull() == null);
+        assert (c1.getThisShouldAlsoBeNull() == null);
+    }
+}
