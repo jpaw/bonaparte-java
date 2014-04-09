3429eac52265d6befd49eb2c4f197003d1a17956
Michael Bischoff <jpaw@online.de>
2014-04-09 22:54:24 +0200

    add test case for externalizable

---
+++ bonaparte-core-test/src/test/java/testcases/io/TestExternalizable.java
@@ -0,0 +1,36 @@
+package testcases.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Externalizable;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.pojos.io.Key;
+import de.jpaw.bonaparte.pojos.io.MyObject;
+
+public class TestExternalizable {
+	
+	private Object runIO(Externalizable org, Class<? extends Externalizable> type) throws Exception {
+		// serialize
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(os);
+		org.writeExternal(oos);
+		oos.flush();
+		byte [] serializedData = os.toByteArray();
+		
+		// deserialize
+		Externalizable inst = type.newInstance();
+		inst.readExternal(new ObjectInputStream(new ByteArrayInputStream(serializedData)));
+		return inst;
+	}
+	
+	
+	@Test
+	public void testExternalizable() throws Exception {
+		MyObject org = new MyObject(new Key("bla!"), "Hello, world");
+		Object moved = runIO(org, MyObject.class);
+		assert(org.equals(moved));
+	}
+}
