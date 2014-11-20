de95d8650931ee24fce220a52d3e64edc84da65f
Michael Bischoff <jpaw@online.de>
2014-11-20 18:45:15 +0100

    upgrade xtend to 2.7.3, add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GOAL=install
+if [ x != x$1 ]; then
+	GOAL=$1
+fi
+
+echo "Cleaning repo folder..."
+git clean -fdx
+
+echo "executing mvn $GOAL..."
+(cd bonaparte-defs && mvn $GOAL)
+(cd bonaparte-base && mvn $GOAL)
