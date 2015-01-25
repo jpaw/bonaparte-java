757e8cbb9ab1adc45b64631e1bd933939ddf2fd2
Michael Bischoff <jpaw@online.de>
2015-01-25 15:26:41 +0100

    bugfix, add testcase for serialization with IDs

---
+++ bonaparte-core-test/src/test/java/testcases/very/compact/TestWithIds.java
@@ -0,0 +1,43 @@
+package testcases.very.compact;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.core.BonaPortable;
+import de.jpaw.bonaparte.core.CompactByteArrayComposer;
+import de.jpaw.bonaparte.core.CompactByteArrayParser;
+import de.jpaw.bonaparte.pojos.very.compact.withIds.Complex;
+import de.jpaw.bonaparte.pojos.very.compact.withIds.Vector;
+import de.jpaw.bonaparte.scanner.BClassScanner;
+import de.jpaw.util.ByteUtil;
+
+public class TestWithIds {
+    @BeforeSuite
+    public void registerClasses() {
+        BClassScanner.init();
+    }
+    
+    @Test
+    public void testCompactWithIds() throws Exception {
+        Complex x = new Complex("AA", 65535);
+        Complex yAndZ = new Complex("aa", 170*256+170);
+        
+        Vector org = new Vector(x, yAndZ, yAndZ);
+        
+        CompactByteArrayComposer cbac = new CompactByteArrayComposer(1000, true);
+        cbac.writeRecord(org);
+        System.out.println("Length with CompactByteArrayComposer (ID) is " + cbac.getBuilder().length());
+        
+        // dump the bytes
+        byte [] data = cbac.getBuilder().getBytes();
+        System.out.println(ByteUtil.dump(data, 100));
+        assert(data.length == 28);  // outer object per PQON, first two complex components as base object, last as repeated.
+        
+        // parse the result
+        CompactByteArrayParser cbap = new CompactByteArrayParser(data, 0, data.length);
+        BonaPortable copy = cbap.readRecord();
+        assert(copy != null);
+        assert(copy instanceof Vector);
+        assert(org.equals(copy));
+    }
+}
--- bonaparte-scanner/src/main/java/de/jpaw/bonaparte/scanner/BClassScanner.java
+++ bonaparte-scanner/src/main/java/de/jpaw/bonaparte/scanner/BClassScanner.java
@@ -4,6 +4,7 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.jpaw.bonaparte.core.BonaPortable;
 import de.jpaw.bonaparte.core.BonaPortableClass;
 import de.jpaw.bonaparte.core.BonaPortableFactoryById;
 
@@ -14,15 +15,20 @@ public class BClassScanner {
         scanAndRegisterBonaPortables("de.jpaw.bonaparte");  // meta and ui might be referenced somewhere...
     }
     
+    /** Entry of no further packages need scanning. */
+    public static void init() {
+    }
+    
+    /** Entry for separate packages. */
     public static void scanAndRegisterBonaPortables(String packageName) {
         int ctr = 0;
-        for (Class<? extends BonaPortableClass> cls : new Reflections(packageName).getSubTypesOf(BonaPortableClass.class)) {
+        for (Class<? extends BonaPortable> cls : new Reflections(packageName).getSubTypesOf(BonaPortable.class)) {
             try {
-                BonaPortableClass<?> bclass = (BonaPortableClass<?>) cls.getMethod("class$BonaPortableClass()").invoke(null);
+                BonaPortableClass<?> bclass = (BonaPortableClass<?>) cls.getMethod("class$BonaPortableClass").invoke(null);
                 if (BonaPortableFactoryById.registerClass(bclass))
                     ++ctr;
             } catch (Exception e) {
-                LOGGER.warn("Cannot initialize xenum {}: {}", cls.getCanonicalName(), e.getMessage());
+                LOGGER.warn("Cannot obtain BonaPortableClass for {}: {}", cls.getCanonicalName(), e.getMessage());
             }
         }
         LOGGER.info("Startup: Loaded {} BonaPortable classes", ctr);
