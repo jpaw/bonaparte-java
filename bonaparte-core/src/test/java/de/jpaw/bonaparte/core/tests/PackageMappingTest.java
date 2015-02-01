d3bfc5c46d86da01e5132c3abe426f54df4c2604
Michael Bischoff <jpaw@online.de>
2015-02-01 12:20:14 +0100

    update package mapping

--- bonaparte-core/src/test/java/de/jpaw/bonaparte/core/tests/PackageMappingTest.java
+++ bonaparte-core/src/test/java/de/jpaw/bonaparte/core/tests/PackageMappingTest.java
@@ -10,16 +10,16 @@ public class PackageMappingTest {
     public void testDefaultPackageMapping() throws Exception {
         assert BonaPortableFactory.mapPackage("myStuff") == null;
         assert BonaPortableFactory.mapPackage("meta.ClassDefinition").equals("de.jpaw.bonaparte.pojos.meta.ClassDefinition");
-        assert BonaPortableFactory.mapPackage("money.Amount").equals("de.jpaw.bonaparte.pojos.money.Amount");
+        assert BonaPortableFactory.mapPackage("refs.Amount").equals("de.jpaw.bonaparte.pojos.refs.Amount");
     }
     
     @Test
     public void testModifiedPackageMapping() throws Exception {
-        assert (BonaPortableFactory.addToPackagePrefixMap("money", null).equals("de.jpaw.bonaparte.pojos.money"));                      // new mapping is "none"
+        assert (BonaPortableFactory.addToPackagePrefixMap("refs", null).equals("de.jpaw.bonaparte.pojos.refs"));                        // new mapping is "none"
         assert(BonaPortableFactory.addToPackagePrefixMap("meta.special", "somewhereelse") == null);                                     // new mapping for subpackage
         assert BonaPortableFactory.mapPackage("myStuff") == null;
         assert BonaPortableFactory.mapPackage("meta.ClassDefinition").equals("de.jpaw.bonaparte.pojos.meta.ClassDefinition");           // unchanged
-        assert BonaPortableFactory.mapPackage("meta.special.Type").equals("somewhereelse.Type");                                       // completely different
-        assert BonaPortableFactory.mapPackage("money.Amount") == null;
+        assert BonaPortableFactory.mapPackage("meta.special.Type").equals("somewhereelse.Type");                                        // completely different
+        assert BonaPortableFactory.mapPackage("refs.Amount") == null;
     }
 }
