9b3dfc5fb7768b325fa556ba83c14201963567e6
Michael Bischoff <jpaw@online.de>
2015-02-28 13:31:07 +0100

    ref generator included in interface

---
+++ bonaparte-core/src/main/java/de/jpaw/bonaparte/core/BonaPortableRef.java
@@ -0,0 +1,15 @@
+package de.jpaw.bonaparte.core;
+
+public interface BonaPortableRef extends BonaPortable {
+    /**
+     * Returns a technical reference of type long which can be used as a primary key in key/value stores.
+     * This is possibly identical to a JPA ID, and possibly different (for entities which do not have a surrogate key). 
+     */
+    long get$RefP();
+    
+    /**
+     * Returns a technical reference of type Long which can be used as a primary key in key/value stores.
+     * This is possibly identical to a JPA ID, and possibly different (for entities which do not have a surrogate key). 
+     */
+    Long get$RefW();
+}
