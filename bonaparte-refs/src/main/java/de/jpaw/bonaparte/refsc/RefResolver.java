665dff85dcd975559540c5ec02878752d79267c8
Michael Bischoff <jpaw@online.de>
2015-03-02 03:31:49 +0100

    add extra refsc methods - part 1

--- bonaparte-refs/src/main/java/de/jpaw/bonaparte/refsc/RefResolver.java
+++ bonaparte-refs/src/main/java/de/jpaw/bonaparte/refsc/RefResolver.java
@@ -1,12 +1,12 @@
 package de.jpaw.bonaparte.refsc;
 
-import de.jpaw.bonaparte.core.BonaPortable;
+import de.jpaw.bonaparte.core.BonaPortableRef;
 import de.jpaw.bonaparte.pojos.api.TrackingBase;
 import de.jpaw.bonaparte.refs.BaseRefResolver;
 import de.jpaw.util.ApplicationException;
 
 /** API to noSQL backends (mini EntityManager) for types inheriting from their key superclass. */
-public interface RefResolver<REF extends BonaPortable, KEY extends REF, DTO extends KEY, TRACKING extends TrackingBase> extends BaseRefResolver<REF, DTO, TRACKING> {
+public interface RefResolver<REF extends BonaPortableRef, KEY extends REF, DTO extends KEY, TRACKING extends TrackingBase> extends BaseRefResolver<REF, DTO, TRACKING> {
     /**
      * Returns the key for the provided unique index. Null-safe, returns 0 for a null parameter. Throws an exception if the reference does not exist.
      */
