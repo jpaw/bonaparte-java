24739fdfa0d81fe6ff2e3f0c735cc461b77739c4
Michael Bischoff <jpaw@online.de>
2015-08-21 20:34:27 +0200

    improve logging for ASCII field autofixes in some composers

--- bonaparte-core/src/main/java/de/jpaw/bonaparte/util/FixASCII.java
+++ bonaparte-core/src/main/java/de/jpaw/bonaparte/util/FixASCII.java
@@ -40,4 +40,36 @@ public class FixASCII {
         return s;
     }
 
+    /**
+     * <code>checkAsciiAndFixIfRequired()</code> tests if a String is indeed an ASCII string and does not exceed the maximum length. It will issue a warning and
+     * replace offending characters with a question mark.
+     *
+     * @param s
+     *            the string to test
+     * @param maxlength
+     *            if > 0, the maximum allowable string length
+     * @return The sanitized string.
+     */
+    public static String checkAsciiAndFixIfRequired(String s, int maxlength, String fieldname) {
+        if ((maxlength > 0) && (s.length() > maxlength)) {
+            logger.warn("Application violating interface specs: trying to send contents for field {} which is too long: {} instead of {} characters: {}",
+                    fieldname, s.length(), maxlength, s);
+            s = s.substring(0, maxlength);
+        }
+        if (!CharTestsASCII.isPrintableOrTab(s)) {
+            // violated a test
+            logger.warn("Application violating interface specs: illegal character in ASCII field {}: {}", fieldname, s);
+            StringBuilder buff = new StringBuilder(s.length());
+            for (int i = 0; i < s.length(); ++i) {
+                char c = s.charAt(i);
+                if (!CharTestsASCII.isAsciiPrintableOrTab(c)) {
+                    buff.append('?');
+                } else {
+                    buff.append(c);
+                }
+            }
+            s = buff.toString();
+        }
+        return s;
+    }
 }
