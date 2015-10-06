02ef1c58fc0852eec0216e8892efcdf7b88167bd
Michael Bischoff <jpaw@online.de>
2015-10-06 20:48:25 +0200

    fix META-INFs to export generated packages, add JSON adapter (using GSON)

---
+++ bonaparte-adapters-gson/src/main/java/de/jpaw/bonaparte/adapters/gson/GsonElementAdapter.java
@@ -0,0 +1,23 @@
+package de.jpaw.bonaparte.adapters.gson;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import de.jpaw.bonaparte.core.ExceptionConverter;
+
+public class GsonElementAdapter {
+    
+    public static String marshal(JsonElement obj) {
+        return obj.toString();
+    }
+
+    public static <E extends Exception> JsonElement unmarshal(String str, ExceptionConverter<E> p) throws E {
+        if (str == null)
+            return null;
+        try {
+            return new JsonParser().parse(str);
+        } catch (Exception e) {
+            throw p.customExceptionConverter("cannot parse JSON Element", e);
+        }
+    }
+}
---
+++ bonaparte-adapters-gson/src/main/java/de/jpaw/bonaparte/adapters/gson/GsonObjectAdapter.java
@@ -0,0 +1,27 @@
+package de.jpaw.bonaparte.adapters.gson;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import de.jpaw.bonaparte.core.ExceptionConverter;
+
+public class GsonObjectAdapter {
+    
+    public static String marshal(JsonObject obj) {
+        return obj.toString();
+    }
+
+    public static <E extends Exception> JsonObject unmarshal(String str, ExceptionConverter<E> p) throws E {
+        if (str == null)
+            return null;
+        try {
+            JsonElement elem = new JsonParser().parse(str);
+            if (elem instanceof JsonObject)
+                return (JsonObject)elem;
+            throw new Exception("Parsed JSON is not an object");
+        } catch (Exception e) {
+            throw p.customExceptionConverter("cannot parse JSON object", e);
+        }
+    }
+}
