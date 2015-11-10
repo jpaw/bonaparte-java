e1e7fa9e17585747f408489417523f0a2b3046c3
Michael Bischoff <jpaw@online.de>
2015-11-10 07:23:03 +0100

    Jwt refinement

--- bonaparte-api/src/main/java/de/jpaw/bonaparte/api/JwtConverter.java
+++ bonaparte-api/src/main/java/de/jpaw/bonaparte/api/JwtConverter.java
@@ -6,6 +6,7 @@ import de.jpaw.bonaparte.core.JsonComposer;
 import de.jpaw.bonaparte.core.MapParser;
 import de.jpaw.bonaparte.pojos.api.auth.JwtAlg;
 import de.jpaw.bonaparte.pojos.api.auth.Jwt;
+import de.jpaw.bonaparte.pojos.api.auth.JwtInfo;
 import de.jpaw.bonaparte.pojos.api.auth.JwtPayload;
 import de.jpaw.json.JsonException;
 import de.jpaw.json.JsonParser;
@@ -14,7 +15,7 @@ import de.jpaw.util.ApplicationException;
 /** Conversions between JSON strings and Bonaparte classes for parts of the JWT. */
 public class JwtConverter {
 
-    public static Map<String,Object> algAsMap(String json) throws JsonException {
+    public static Map<String,Object> asMap(String json) throws JsonException {
         return new JsonParser(json, true).parseObject();
     }
     
@@ -36,5 +37,35 @@ public class JwtConverter {
     
     public static String toJson(JwtPayload payload) {
         return JsonComposer.toJsonString(payload);
+    }
+    
+    
+    public static JwtInfo parseJwtInfo(String json) throws ApplicationException {
+        final JwtPayload payload = parsePayload(json);      // use the intermediate object to avoid the hassle of handwritten type checks
+        final JwtInfo info = new JwtInfo();
+        info.setIssuer              (payload.getIss());
+        info.setUserId              (payload.getSub());
+        info.setAudience            (payload.getAud());
+        info.setExpiresAt           (payload.getExp());
+        info.setNotBefore           (payload.getNbf());
+        info.setIssuedAt            (payload.getIat());
+        info.setJsonTokenIdentifier(payload.getJti());
+
+        info.setName                (payload.getName());
+        info.setLocale              (payload.getLocale());
+        info.setZoneinfo            (payload.getZoneinfo());
+        
+        info.setTenantId            (payload.getI());
+        info.setTenantRef           (payload.getT());
+        info.setSessionRef          (payload.getS());
+        info.setUserRef             (payload.getU());
+        info.setRoleRef             (payload.getR());
+        info.setLogLevel            (payload.getL());
+        info.setLogLevelErrors      (payload.getE());
+        info.setResource            (payload.getP());
+        info.setPermissionsMin      (payload.getPl());
+        info.setPermissionsMax      (payload.getPu());
+        info.freeze();
+        return info;
     }
 }
