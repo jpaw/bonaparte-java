ec8810f7552b40aafd08c0310f1763788013055d
Michael Bischoff <jpaw@online.de>
2016-01-30 11:40:40 +0100

    add enumSet and xenumSet test cases

---
+++ bonaparte-core-test/src/test/java/testcases/xenum/TestStringEnumSetOperations.java
@@ -0,0 +1,108 @@
+package testcases.xenum;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.pojos.testXenum.AbcSet;
+
+public class TestStringEnumSetOperations {
+    
+    private void testUnify(String a, String b, String union) throws Exception {
+        AbcSet x = new AbcSet(a);
+        x.unifyWith(new AbcSet(b));                 // Set union
+        
+        Assert.assertEquals(x, new AbcSet(union));  // Set equals
+        
+        AbcSet x1 = new AbcSet(a);
+        x1.unifyWith(b);                            // String arg union
+        
+        Assert.assertEquals(x1.getBitmap(), union); // String equals
+    }
+    
+    @Test
+    public void testEnumSetUnion() throws Exception {
+        testUnify("AB", "BC", "ABC");
+        testUnify("AB", "C",  "ABC");
+        testUnify("AB", "B",  "AB");
+        testUnify("AB", "A",  "AB");
+        testUnify("BC", "AB", "ABC");
+        testUnify("B",  "C",  "BC");
+        testUnify("B",  "AB", "AB");
+        testUnify("AB", "AB", "AB");
+        testUnify("AB", "",   "AB");
+        testUnify("",   "AB", "AB");
+    }
+    
+    
+    private void testIntersect(String a, String b, String common) throws Exception {
+        AbcSet x = new AbcSet(a);
+        x.intersectWith(new AbcSet(b));             // Set Difference
+        
+        Assert.assertEquals(x, new AbcSet(common)); // Set equals
+        
+        AbcSet x1 = new AbcSet(a);
+        x1.intersectWith(b);                        // String arg Difference
+        
+        Assert.assertEquals(x1.getBitmap(), common);// String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetIntersect() throws Exception {
+        testIntersect("AB", "BC", "B");
+        testIntersect("AB", "C",  "");
+        testIntersect("AB", "B",  "B");
+        testIntersect("AB", "A",  "A");
+        testIntersect("BC", "AB", "B");
+        testIntersect("B",  "C",  "");
+        testIntersect("B",  "AB", "B");
+        testIntersect("AB", "AB", "AB");
+        testIntersect("AB", "",   "");
+        testIntersect("",   "AB", "");
+    }
+
+    private void testDifference(String a, String b, String difference) throws Exception {
+        AbcSet x = new AbcSet(a);
+        x.exclude(new AbcSet(b));                       // Set exclusion
+        
+        Assert.assertEquals(x, new AbcSet(difference)); // Set equals
+        
+        AbcSet x1 = new AbcSet(a);
+        x1.exclude(b);                                  // String arg exclusion
+        
+        Assert.assertEquals(x1.getBitmap(), difference);// String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetDifference() throws Exception {
+        testDifference("AB", "BC", "A");
+        testDifference("AB", "C",  "AB");
+        testDifference("AB", "B",  "A");
+        testDifference("AB", "A",  "B");
+        testDifference("BC", "AB", "C");
+        testDifference("B",  "C",  "B");
+        testDifference("B",  "AB", "");
+        testDifference("AB", "AB", "");
+        testDifference("AB", "",   "AB");
+        testDifference("",   "AB", "");
+        testDifference("ABC", "B", "AC");
+        testDifference("ABC", "AC", "B");
+    }
+
+    private void testInit(String a, String result) throws Exception {
+        AbcSet x = new AbcSet(a);
+        Assert.assertEquals(x.getBitmap(), result);    // String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetInit() throws Exception {
+        testInit("AB", "AB");
+        testInit("",   "");
+        testInit("BA", "AB");
+        testInit("ACB", "ABC");
+        testInit("CAB", "ABC");
+        testInit("CBA", "ABC");
+    }
+}
---
+++ bonaparte-core-test/src/test/java/testcases/xenum/TestStringXEnumSetOperations.java
@@ -0,0 +1,108 @@
+package testcases.xenum;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.pojos.testXenum.XAlphabet;
+
+public class TestStringXEnumSetOperations {
+    
+    private void testUnify(String a, String b, String union) throws Exception {
+        XAlphabet x = new XAlphabet(a);
+        x.unifyWith(new XAlphabet(b));                 // Set union
+        
+        Assert.assertEquals(x, new XAlphabet(union));  // Set equals
+        
+        XAlphabet x1 = new XAlphabet(a);
+        x1.unifyWith(b);                            // String arg union
+        
+        Assert.assertEquals(x1.getBitmap(), union); // String equals
+    }
+    
+    @Test
+    public void testEnumSetUnion() throws Exception {
+        testUnify("AB", "BC", "ABC");
+        testUnify("AB", "C",  "ABC");
+        testUnify("AB", "B",  "AB");
+        testUnify("AB", "A",  "AB");
+        testUnify("BC", "AB", "ABC");
+        testUnify("B",  "C",  "BC");
+        testUnify("B",  "AB", "AB");
+        testUnify("AB", "AB", "AB");
+        testUnify("AB", "",   "AB");
+        testUnify("",   "AB", "AB");
+    }
+    
+    
+    private void testIntersect(String a, String b, String common) throws Exception {
+        XAlphabet x = new XAlphabet(a);
+        x.intersectWith(new XAlphabet(b));             // Set Difference
+        
+        Assert.assertEquals(x, new XAlphabet(common)); // Set equals
+        
+        XAlphabet x1 = new XAlphabet(a);
+        x1.intersectWith(b);                        // String arg Difference
+        
+        Assert.assertEquals(x1.getBitmap(), common);// String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetIntersect() throws Exception {
+        testIntersect("AB", "BC", "B");
+        testIntersect("AB", "C",  "");
+        testIntersect("AB", "B",  "B");
+        testIntersect("AB", "A",  "A");
+        testIntersect("BC", "AB", "B");
+        testIntersect("B",  "C",  "");
+        testIntersect("B",  "AB", "B");
+        testIntersect("AB", "AB", "AB");
+        testIntersect("AB", "",   "");
+        testIntersect("",   "AB", "");
+    }
+
+    private void testDifference(String a, String b, String difference) throws Exception {
+        XAlphabet x = new XAlphabet(a);
+        x.exclude(new XAlphabet(b));                       // Set exclusion
+        
+        Assert.assertEquals(x, new XAlphabet(difference)); // Set equals
+        
+        XAlphabet x1 = new XAlphabet(a);
+        x1.exclude(b);                                  // String arg exclusion
+        
+        Assert.assertEquals(x1.getBitmap(), difference);// String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetDifference() throws Exception {
+        testDifference("AB", "BC", "A");
+        testDifference("AB", "C",  "AB");
+        testDifference("AB", "B",  "A");
+        testDifference("AB", "A",  "B");
+        testDifference("BC", "AB", "C");
+        testDifference("B",  "C",  "B");
+        testDifference("B",  "AB", "");
+        testDifference("AB", "AB", "");
+        testDifference("AB", "",   "AB");
+        testDifference("",   "AB", "");
+        testDifference("ABC", "B", "AC");
+        testDifference("ABC", "AC", "B");
+    }
+
+    private void testInit(String a, String result) throws Exception {
+        XAlphabet x = new XAlphabet(a);
+        Assert.assertEquals(x.getBitmap(), result);    // String equals
+    }
+    
+    
+    @Test
+    public void testEnumSetInit() throws Exception {
+        testInit("AB", "AB");
+        testInit("",   "");
+        testInit("BA", "AB");
+        testInit("ACB", "ABC");
+        testInit("CAB", "ABC");
+        testInit("CBA", "ABC");
+    }
+}
