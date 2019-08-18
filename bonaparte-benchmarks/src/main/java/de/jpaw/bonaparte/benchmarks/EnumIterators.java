3cd55d6322350614c65c54c79068b207a8d26693
Michael Bischoff <jpaw@online.de>
2019-08-18 11:10:15 +0200

    upgrade dependencies and add new benchmark and test

---
+++ bonaparte-benchmarks/src/main/java/de/jpaw/bonaparte/benchmarks/EnumIterators.java
@@ -0,0 +1,57 @@
+package de.jpaw.bonaparte.benchmarks;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import de.jpaw.bonaparte.pojos.enumtest.AlphabetEnum;
+
+// results:
+// java -jar target/bonaparte-benchmarks.jar -i 3 -f 3 -wf 1 -wi 3 ".*EnumIterators.*"
+
+//Benchmark                   Mode  Cnt   Score   Error  Units
+//EnumIterators.empty         avgt    9  46.237 ± 0.040  ns/op
+//EnumIterators.oldSchool     avgt    9  68.261 ± 0.038  ns/op
+//EnumIterators.usual         avgt    9  75.332 ± 0.192  ns/op
+//EnumIterators.newGenerated  avgt    9  67.750 ± 0.139  ns/op
+
+@State(value = Scope.Thread)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+public class EnumIterators {
+
+    @Benchmark
+    public void empty(Blackhole bh) {
+        for (int i = 0; i < 26; ++i) {
+            bh.consume(i);
+        }
+    }
+
+    @Benchmark
+    public void oldSchool(Blackhole bh) {
+        for (int i = 0; i < 26; ++i) {
+            AlphabetEnum a = AlphabetEnum.valueOf(i);
+            bh.consume(a.name());
+        }
+    }
+    
+    @Benchmark
+    public void usual(Blackhole bh) {
+        for (AlphabetEnum a: AlphabetEnum.values()) {
+            bh.consume(a.name());
+        }
+    }
+    
+    @Benchmark
+    public void newGenerated(Blackhole bh) {
+        for (AlphabetEnum a: AlphabetEnum.all) {
+            bh.consume(a.name());
+        }
+    }
+}
---
+++ bonaparte-core-test/src/test/java/testcases/enumMemory/TestIterators.java
@@ -0,0 +1,75 @@
+package testcases.enumMemory;
+
+import org.testng.annotations.Test;
+
+import de.jpaw.bonaparte.pojos.enumtest.AlphabetEnum;
+
+public class TestIterators {
+    private static final int NUM_ITERATIONS = 100;
+
+    private final StringBuffer buff = new StringBuffer(100000);
+    private long memSize;
+
+    public void oldSchool() {
+        for (int i = 0; i < 26; ++i) {
+            AlphabetEnum a = AlphabetEnum.valueOf(i);
+            buff.append(a.name());
+        }
+    }
+    
+    public void usual() {
+        for (AlphabetEnum a: AlphabetEnum.values()) {
+            buff.append(a.name());
+        }
+    }
+    
+    public void newGenerated() {
+        for (AlphabetEnum a: AlphabetEnum.all) {
+            buff.append(a.name());
+        }
+    }
+    
+    private void before() {
+        System.gc();
+        memSize = Runtime.getRuntime().freeMemory();
+    }
+    private long after(String which) {
+        long memUsed = memSize - Runtime.getRuntime().freeMemory();
+        System.out.println("Memory used for iteration for " + which + " is " + memUsed);
+        return memUsed;
+    }
+
+
+    @Test
+    public void testOldSchool() {
+        oldSchool();  // load classes
+        before();
+        for (int i = 0; i < NUM_ITERATIONS; ++i) {
+            oldSchool();
+            buff.setLength(0);
+        }
+        after("oldSchool");
+    }
+
+    @Test
+    public void testUsual() {
+        usual();  // load classes
+        before();
+        for (int i = 0; i < NUM_ITERATIONS; ++i) {
+            usual();
+            buff.setLength(0);
+        }
+        after("usual");
+    }
+
+    @Test
+    public void testNewGenerated() {
+        newGenerated();  // load classes
+        before();
+        for (int i = 0; i < NUM_ITERATIONS; ++i) {
+            newGenerated();
+            buff.setLength(0);
+        }
+        after("newGenerated");
+    }
+}
