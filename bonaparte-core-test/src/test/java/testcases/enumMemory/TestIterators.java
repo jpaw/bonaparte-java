9fb5e0886e0108d6a7943a08e8c59d84cd41e734
Michael Bischoff <jpaw@online.de>
2019-08-18 12:23:15 +0200

    check memory consumption - with ThreadMXBean, because Runtime memory functions did not work

--- bonaparte-core-test/src/test/java/testcases/enumMemory/TestIterators.java
+++ bonaparte-core-test/src/test/java/testcases/enumMemory/TestIterators.java
@@ -1,14 +1,46 @@
 package testcases.enumMemory;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 import org.testng.annotations.Test;
 
 import de.jpaw.bonaparte.pojos.enumtest.AlphabetEnum;
 
+// tests memory allocation per method to iterate enums
+//Memory used for iteration for oldSchool is 48
+//Memory used for iteration for newGenerated is 48
+//Memory used for iteration for usual is 120048 -- due to defensive array copy in Enum.values()
+
 public class TestIterators {
-    private static final int NUM_ITERATIONS = 100;
+    private static final int NUM_ITERATIONS = 1000;
 
     private final StringBuffer buff = new StringBuffer(100000);
     private long memSize;
+    private ThreadMXBean tmb;
+            
+    private long getMemory() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+        }
+        tmb = ManagementFactory.getThreadMXBean();
+        com.sun.management.ThreadMXBean sb = (com.sun.management.ThreadMXBean)tmb;
+        long threadId = Thread.currentThread().getId();
+        return sb.getThreadAllocatedBytes(threadId);
+    }
+    
+    private void before() {
+        System.gc();
+        memSize = getMemory();
+    }
+    private long after(String which) {
+        long memUsed = getMemory() - memSize;
+        System.out.println("Memory used for iteration for " + which + " is " + memUsed);
+        return memUsed;
+    }
+
 
     public void oldSchool() {
         for (int i = 0; i < 26; ++i) {
@@ -28,18 +60,8 @@ public class TestIterators {
             buff.append(a.name());
         }
     }
+
     
-    private void before() {
-        System.gc();
-        memSize = Runtime.getRuntime().freeMemory();
-    }
-    private long after(String which) {
-        long memUsed = memSize - Runtime.getRuntime().freeMemory();
-        System.out.println("Memory used for iteration for " + which + " is " + memUsed);
-        return memUsed;
-    }
-
-
     @Test
     public void testOldSchool() {
         oldSchool();  // load classes
@@ -71,5 +93,21 @@ public class TestIterators {
             buff.setLength(0);
         }
         after("newGenerated");
+    }
+    
+    public static void main(String [] args) {
+        TestIterators me = new TestIterators();
+        // first iteration - ignore, preloading
+        me.testOldSchool();
+        me.testNewGenerated();
+        me.testUsual();
+        // second iteration - the relevant result 
+        me.testOldSchool();
+        me.testNewGenerated();
+        me.testUsual();
+        // third iteration - should be the same result 
+        me.testOldSchool();
+        me.testNewGenerated();
+        me.testUsual();
     }
 }
