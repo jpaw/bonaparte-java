c7b2657f8a989434744ad06033fd2732f1d7c0e5
Michael Bischoff <Michael.Bischoff@Bertelsmann.de>
2024-05-03 19:51:54 +0200

    DSL-17: support for additional extension

--- bonaparte-api-media/src/main/java/de/jpaw/bonaparte/api/media/MediaTypeInfo.java
+++ bonaparte-api-media/src/main/java/de/jpaw/bonaparte/api/media/MediaTypeInfo.java
@@ -1,7 +1,10 @@
 package de.jpaw.bonaparte.api.media;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
+import com.google.common.collect.ImmutableList;
 
 import de.jpaw.bonaparte.core.MimeTypes;
 import de.jpaw.bonaparte.pojos.api.media.MediaCategory;
@@ -9,31 +12,59 @@ import de.jpaw.bonaparte.pojos.api.media.MediaType;
 import de.jpaw.bonaparte.pojos.api.media.MediaTypeDescriptor;
 import de.jpaw.bonaparte.pojos.api.media.MediaXType;
 import de.jpaw.enums.XEnumFactory;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
+/** Registry for media types. */
 public class MediaTypeInfo {
+    /** Maps the MIME type to the descriptor. */
     private static final ConcurrentMap<String, MediaTypeDescriptor> mimeMap = new ConcurrentHashMap<String, MediaTypeDescriptor>(32);
+
+    /** Maps the Bonaparte (X)enum to the descriptor. */
     private static final ConcurrentMap<MediaXType, MediaTypeDescriptor> reverseMimeMap = new ConcurrentHashMap<MediaXType, MediaTypeDescriptor>(32);
+
+    /** Maps the File extension to the descriptor. */
+    private static final ConcurrentMap<String, MediaTypeDescriptor> extensionMap = new ConcurrentHashMap<String, MediaTypeDescriptor>(32);
+
+    /** File extensions of various popular programming languages, used to map to text/plain. */
+    private static final List<String> PROGRAMMING_LANGUAGE_EXTENSIONS = ImmutableList.of(
+        "c", "h", "cpp", "cs", "m",         // C, C++, C#, Objective-C
+        "java", "xtend", "js", "ts", "zul", // Java, Xtend, JavaScript, TypeScript, ZK screen definitions
+        "py", "rs", "go",                   // Python, Rust, Go
+        "sql", "sh", "csh", "ksh",          // SQL, Shell, C-Shell, KShell
+        "bon", "bddl",                      // Bonaparte sources
+        "pl", "php"                         // Perl, PHP
+    );
+
+    /** File extensions for various Java runtime bundles. */
+    private static final List<String> JAVA_EXTENSIONS = ImmutableList.of("jar", "war");
 
     protected static void registerFormatType(MediaTypeDescriptor f) {
         f.freeze();  // make it immutable
         reverseMimeMap.putIfAbsent(f.getMediaType(), f);
         mimeMap.putIfAbsent(f.getMimeType(), f);
+        extensionMap.putIfAbsent(f.getDefaultFileExtension(), f);
+        if (f.getAdditionalExtensions() != null) {
+            for (final String ext : f.getAdditionalExtensions()) {
+                extensionMap.putIfAbsent(ext, f);
+            }
+        }
     }
 
     // static initializations
     static {
         final XEnumFactory<MediaXType> fct = MediaXType.myFactory;                         // fileExt,  category,              isText, mimeType
-        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.BONAPARTE),          "bon",  MediaCategory.RECORDS,  true,  MimeTypes.MIME_TYPE_BONAPARTE));
+        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.BONAPARTE),          "b",    MediaCategory.RECORDS,  true,  MimeTypes.MIME_TYPE_BONAPARTE));         // FIXME: made-up MIME type, should have been application/vnd.jpaw-bonaparte
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.XML),                "xml",  MediaCategory.RECORDS,  true,  MimeTypes.MIME_TYPE_XML));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.JSON),               "json", MediaCategory.RECORDS,  true,  MimeTypes.MIME_TYPE_JSON));
-        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.COMPACT_BONAPARTE),  "cb",   MediaCategory.RECORDS,  false, MimeTypes.MIME_TYPE_COMPACT_BONAPARTE));
+        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.COMPACT_BONAPARTE),  "cb",   MediaCategory.RECORDS,  false, MimeTypes.MIME_TYPE_COMPACT_BONAPARTE)); // FIXME: made-up MIME type, should have been application/vnd.jpaw-compact-bonaparte
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.YAML),               "yaml", MediaCategory.RECORDS,  true,  "application/yaml"));  // since Feb-2024: https://www.rfc-editor.org/rfc/rfc9512.html
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.XLS),                "xls",  MediaCategory.RECORDS,  false, "application/vnd.ms-excel"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.XLSX),               "xlsx", MediaCategory.RECORDS,  false, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.PPTX),               "pptx", MediaCategory.DOCUMENT, false, "application/vnd.openxmlformats-officedocument.presentationml.presentation"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.DOCX),               "docx", MediaCategory.DOCUMENT, false, "application/vnd.openxmlformats-officedocument.wordprocessingml.document"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.CSV),                "csv",  MediaCategory.RECORDS,  true,  MimeTypes.MIME_TYPE_CSV));
-        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.TEXT),               "txt",  MediaCategory.TEXT,     true,  "text/plain"));
+        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.TEXT),               "txt",  MediaCategory.TEXT,     true,  "text/plain", PROGRAMMING_LANGUAGE_EXTENSIONS));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.HTML),               "html", MediaCategory.TEXT,     true,  "text/html"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.XHTML),              "xhtml",MediaCategory.TEXT,     true,  "application/xhtml+xml"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.CSS),                "css",  MediaCategory.TEXT,     true,  "text/css"));
@@ -56,17 +87,27 @@ public class MediaTypeInfo {
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.MPG),                "mpg",  MediaCategory.VIDEO,    false, "video/mpeg"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.RAW),                "dat",  MediaCategory.OTHER,    false, "application/octet-stream"));
         // add some archives
-        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.ZIP),                "zip",  MediaCategory.CONTAINER, false, "application/zip"));
+        registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.ZIP),                "zip",  MediaCategory.CONTAINER, false, "application/zip", JAVA_EXTENSIONS));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.GZIP),               "gz",   MediaCategory.CONTAINER, false, "application/gzip"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.ZIP7),               "7z",   MediaCategory.CONTAINER, false, "application/x-7z-compressed"));
         registerFormatType(new MediaTypeDescriptor(fct.getByEnum(MediaType.TAR),                "tar",  MediaCategory.CONTAINER, false, "application/x-tar"));
     }
 
-    public static MediaTypeDescriptor getFormatByMimeType(String mimeType) {
+    /** Returns the MediaTypeDescriptor for a given MIME type, or returns null if none found. */
+    @Nullable
+    public static MediaTypeDescriptor getFormatByMimeType(@Nonnull final String mimeType) {
         return mimeMap.get(mimeType);
     }
 
-    public static MediaTypeDescriptor getFormatByType(MediaXType formatType) {
+    /** Returns the MediaTypeDescriptor for a given bonaparte type, or returns null if none found. */
+    @Nullable
+    public static MediaTypeDescriptor getFormatByType(@Nonnull final MediaXType formatType) {
         return reverseMimeMap.get(formatType);
+    }
+
+    /** Returns the MediaTypeDescriptor for a given file extension. */
+    @Nullable
+    public static MediaTypeDescriptor getFormatByFileExtension(@Nonnull final String mimeType) {
+        return extensionMap.get(mimeType);
     }
 }
